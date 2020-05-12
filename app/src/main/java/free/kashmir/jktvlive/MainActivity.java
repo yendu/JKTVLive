a0dc62c5317b81a4b9353d97403525dbea9cfa81
yonatan <yendu669@gmail.com>
2020-05-12 18:21:09 +0300

    Fix the orientation problem

--- app/src/main/java/free/kashmir/jktvlive/MainActivity.java
+++ app/src/main/java/free/kashmir/jktvlive/MainActivity.java
@@ -1,13 +1,14 @@
 package free.kashmir.jktvlive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         String path = "android.resource://" + getPackageName() + "/" + R.raw.jktv;
         videoView.setVideoURI(Uri.parse(path));
 
+
+
+        if(savedInstanceState!=null){
+            videoView.seekTo(savedInstanceState.getInt("currentTime"));
+
+        }
+//
+
+
+
+
         videoView.start();
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
@@ -31,13 +43,51 @@ public class MainActivity extends AppCompatActivity {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Intent intent=new Intent(MainActivity.this,WebViewActivity.class);
-                        startActivity(intent);
+                            Intent intent=new Intent(MainActivity.this,WebViewActivity.class);
+                            intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            startActivity(intent);
+//                        webView.setVisibility(View.VISIBLE);
+//                        videoView.setVisibility(View.GONE);
+//                        videoView=null;
+//                        webView.loadUrl("https://jktv.live/");
                         //  intent.
                     }
                 },duration);
             }
         });
 
+        }
+
+
+
+
+
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+//        if(webView!=null){
+//            webView.restoreState(savedInstanceState);
+//        }
+
+
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("currentTime",videoView.getCurrentPosition());
+
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+
+
+
+
 }
--- app/src/main/java/free/kashmir/jktvlive/WebViewActivity.java
+++ app/src/main/java/free/kashmir/jktvlive/WebViewActivity.java
@@ -1,24 +1,55 @@
 package free.kashmir.jktvlive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+
 import android.os.Bundle;
-import android.webkit.WebView;
+
+import android.widget.FrameLayout;
+
+import com.rw.videowebview.VideoWebview;
 
 public class WebViewActivity extends AppCompatActivity {
 
-    WebView webView;
+
+
+    VideoWebview videoWebview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-        webView=findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setAppCacheEnabled(true);
-//        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setSaveFormData(true);
-        webView.loadUrl("https://jktv.live/");
+        videoWebview = findViewById(R.id.webView);
+
+        if(savedInstanceState==null){
+            videoWebview.getWebView().loadUrl("https://jktv.live/");
+
+            videoWebview.setFullScreenView(getSupportActionBar(), (FrameLayout) findViewById(R.id.fullscreen_view));
+        }else{
+            videoWebview.getWebView().restoreState(savedInstanceState);
+        }
 
 
     }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        videoWebview.getWebView().restoreState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        videoWebview.getWebView().saveState(outState);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
 }
+
